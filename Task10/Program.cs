af255d5710083cdb251ddced975f41497e28bef5
Molochko1990 <Es01@bk.ru>
2022-11-26 12:15:50 +0500

    Сделал все задания

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int number = random.Next(100, 1000);
+Console.WriteLine($"Random number = {number}");
+int a = number / 10;
+Console.WriteLine(a);
+int b = a % 10;
+Console.WriteLine(b);
--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int number = random.Next(10, 10001);
+Console.WriteLine($"Random number = {number}");
+var str = number.ToString();
+if (str.Length < 3)
+    Console.WriteLine("Третьей цифры нет");
+else
+    Console.WriteLine(str[2]);
